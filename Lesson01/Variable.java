faf83ed17b38cf5cf2b1f3cd7b7708341b3e5f91
Nicolay Poymanov <n.poymanov@gmail.com>
2019-02-28 20:08:51 +0300

    Задание 1-6

---
+++ Lesson01/Variable.java
@@ -0,0 +1,22 @@
+public class Variable {	
+    public static void main(String[] args) {
+
+    	byte cores = 2;
+    	short ram = 8;
+    	int hdd = 200;
+    	long powerSupply = 700;
+    	float sound = 5.1f;
+    	double cpu = 3.1;
+    	char usbType = 'c';
+    	boolean isModern = true;
+
+        System.out.println(cores);
+        System.out.println(ram);
+        System.out.println(hdd);
+        System.out.println(powerSupply);
+        System.out.println(sound);
+        System.out.println(cpu);
+        System.out.println(usbType);
+        System.out.println(isModern);
+    }
+}
