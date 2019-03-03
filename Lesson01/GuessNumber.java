ed22464a9b54611c1ae849a253744fab61c8e974
Nicolay Poymanov <n.poymanov@gmail.com>
2019-03-03 23:49:53 +0300

    Поправлено задание 11

--- Lesson01/GuessNumber.java
+++ Lesson01/GuessNumber.java
@@ -2,16 +2,16 @@ public class GuessNumber {
 
     public static void main(String[] args) {
         int computerVariant = 50;
-        int userVariant = 40;
+        int userVariant = 60;
 
         while (computerVariant != userVariant) {
             if (userVariant > computerVariant) {
                 System.out.println("The number you entered is greater than what the computer wished for");
+                userVariant--;
             } else {
                 System.out.println("The number you entered is less than what the computer wished for");
+                userVariant++;
             }
-
-            userVariant++;
         }
 
         System.out.println("You guessed it!");
