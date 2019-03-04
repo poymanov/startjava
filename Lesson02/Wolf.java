45d9028f209b64fbd36f21ee4c42033983535e07
Nicolay Poymanov <n.poymanov@gmail.com>
2019-03-04 23:36:11 +0300

    Задание 2-3

---
+++ Lesson02/Wolf.java
@@ -0,0 +1,27 @@
+public class Wolf {
+	String sex;
+	String name;
+	float weight;
+	int age;
+	String color;
+
+	void go() {
+		System.out.println("Wolf moves");
+	}
+
+	void sit() {
+		System.out.println("Wolf sits");	
+	}	
+
+	void run() {
+		System.out.println("Wolf runs");	
+	}
+
+	void howl() {
+		System.out.println("Wolf howls");	
+	}
+
+	void hunt() {
+		System.out.println("Wolf hunts");	
+	}
+}
