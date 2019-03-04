04d6f2d7db6dfeae2413c6ca58df905e53c8f8eb
Nicolay Poymanov <n.poymanov@gmail.com>
2019-03-04 22:55:50 +0300

    Задание 2-2

---
+++ Lesson02/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+	String sex = "male";
+	String name = "John";
+	float height = 1.7f;
+	float weight = 71.6f;
+	int age = 25;
+
+	void move() {
+
+	}
+
+	void sit() {
+		
+	}
+
+	void run() {
+		
+	}	
+
+	String speak() {
+		return "Something";
+	}	
+
+	void learn() {
+
+	}
+}
