48ab2e73938a3591842962e9ffe980943c828b2f
Nicolay Poymanov <n.poymanov@gmail.com>
2019-03-06 21:51:16 +0300

    Задания 2-7

---
+++ Lesson02/GuessNumberTest.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class GuessNumberTest {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("First player name is:");    
+		String firstPlayerName = scan.nextLine();
+
+		System.out.println("Second player name is:");    
+		String secondPlayerName = scan.nextLine();
+
+		Player firstPlayer = new Player(firstPlayerName);
+		Player secondPlayer = new Player(secondPlayerName);
+
+		GuessNumber game = new GuessNumber(firstPlayer, secondPlayer);
+		game.play();
+	}
+}
