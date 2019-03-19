aeff7651e444e1e9b75e88cd3821f8f9ec5d7bf9
poymanov <n.poymanov@gmail.com>
2019-03-19 23:49:20 +0300

    Исправление замечений (Задание 5)

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -1,1 +1,14 @@
 CREATE DATABASE jaegers;
+
+\c jaegers
+
+CREATE TABLE robots (
+    id SERIAL PRIMARY KEY,
+    mark char(6) NOT NULL,
+    height REAL NOT NULL,
+    weight REAL NOT NULL,
+    status char(9) NOT NULL,
+    origin TEXT NOT NULL,
+    launch DATE NOT NULL,
+    kaijuKill INTEGER
+);
